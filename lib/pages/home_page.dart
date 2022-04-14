c2b5559159095eb42239e3a27a327c3aab136139
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-04-14 17:46:32 +0600

    Item card create

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,29 @@
+import 'package:days_of_flutter/models/catalog.dart';
 import 'package:days_of_flutter/widgets/mydrawer.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Fluttery Day"),
       ),
       drawer: const MyDrawer(),
-      body: Center(
-        child: Container(
-          child: const Text("Welcome to fluttery day"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
         ),
       ),
     );
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:days_of_flutter/models/catalog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item? item;
+  const ItemWidget({
+    Key? key,
+    this.item,
+  })  : assert(item != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item!.name} pressed");
+        },
+        leading: Image.network("${item!.image}"),
+        title: Text("${item!.name}"),
+        subtitle: Text("${item!.desc}"),
+        trailing: Text(
+          "\$${item!.price}",
+          textScaleFactor: 1.5,
+          style: const TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
