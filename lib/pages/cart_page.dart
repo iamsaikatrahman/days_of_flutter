10d16bfced2400f4e0f80f208e7cdad86ba0943d
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-04-26 21:24:51 +0600

    cart demo page create

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -15,10 +15,70 @@ class CartPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Placeholder().p32(),
-          Divider(),
+          const _CartList().p32().expand(),
+          const Divider(),
+          const _CartTotal(),
         ],
       ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: "Buying is not supported yet".text.make(),
+                ),
+              );
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 31, 21, 66),
+              ),
+            ),
+            child: "Buy".text.xl.bold.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.done),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.remove_circle_outline),
+          ),
+          title: "Item 1".text.make(),
+        );
+      },
     );
   }
 }
